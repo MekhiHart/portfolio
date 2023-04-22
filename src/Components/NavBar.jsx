57067441eb84071d1af3e8fa7fd1518c1fb0da2b
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-22 13:59:39 -0700

    replaced navbar now

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,10 +3,14 @@ export default function NavBar(){
     return(
         <nav>
             <a>Skills</a>
-            <a>Skills</a>
-            <a>Skills</a>
-            <a>Skills</a>
-            <a>Skills</a>
+            <a>Experience</a>
+            <a>Projects</a>
+            <a>About</a>
+            <a>Contact</a>
+            
+            
+            
+            
         </nav>
     )
 }
