9e905c2dec6408ca8b057a84cf6dbf45da4826f9
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-22 11:09:56 -0700

    first commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero(){
+    return(
+    <div>
+        <h1>Mekhi Hart Dela Cruz.</h1>
+        <h2>Full-Stack Software Developer</h2>
+        <h3>California State Long Beach</h3>
+        <h4>B.S. Computer Science | August 2020 - May 2024</h4>
+    </div>
+    )
+}
