47b5a7d1c31b328d429b3325b1357cf4861d4676
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-22 13:36:46 -0700

    swapped links for navbar, still switching navbar

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
+import {faFile} from '@fortawesome/free-solid-svg-icons'
+
 export default function Hero(){
     return(
     <div className="hero--body fullScreen">
@@ -6,6 +10,12 @@ export default function Hero(){
             <h2>Full-Stack Software Developer</h2>
             <h3 style={{marginBottom: "-20px"}}>California State University Long Beach</h3>
             <h4>B.S. <i>Computer Science</i> | August 2020 - May 2024</h4>
+        </div>
+
+        <div className="hero--links" style={{marginTop:"50px"}}>
+            <a href="" target='_blank'><FontAwesomeIcon icon={faGithub} size='2x'/></a>
+            <a href="" target='_blank'><FontAwesomeIcon icon={faLinkedin} size='2x'/></a>
+            <a href="" target='_blank'><FontAwesomeIcon icon={faFile} size='2x'/></a>
         </div>
 
     </div>
