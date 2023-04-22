ce9b1ebd2a90970441b87de49f65da2a628c1bb0
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-22 13:56:26 -0700

    arranged navbar correctly

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero(){
             <h1 style={{marginBottom: "-20px" , fontWeight:"bold"}}>Mekhi Hart Dela Cruz</h1>
             <h2>Full-Stack Software Developer</h2>
             <h3 style={{marginBottom: "-20px"}}>California State University Long Beach</h3>
-            <h4>B.S. <i>Computer Science</i> | August 2020 - May 2024</h4>
+            <h4>B.S. in <i>Computer Science</i> | August 2020 - May 2024</h4>
         </div>
 
         <div className="hero--links" style={{marginTop:"50px"}}>
