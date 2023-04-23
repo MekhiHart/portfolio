eb9db9afcb82d42bb5d6eb1a6a8174d145473cbd
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-23 12:03:06 -0700

    centered popUp container

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -2,6 +2,9 @@ export default function PopUp(){
     return(
         <div className="popup--overlay">
             <div className="popUp--container">
+                <div className="popUp--tabs">
+                    
+                </div>
                 <h1>Hello</h1>
                 <h2>This is Mekhi</h2>
             </div>
