101dbdc22ae48c0e9aa21b3e1c2e62cfa55a0c66
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-23 12:22:33 -0700

    added tabs on popup container

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -1,9 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faX} from '@fortawesome/free-solid-svg-icons'
+
 export default function PopUp(){
     return(
         <div className="popup--overlay">
             <div className="popUp--container">
                 <div className="popUp--tabs">
-                    
+                    <button>Skills</button>
+                    <button>Skills</button>
+                    <button>Skills</button>
+                    <button>Skills</button>
+                    <button id="closePopUp"><FontAwesomeIcon icon={faX} size='2x'/></button>
                 </div>
                 <h1>Hello</h1>
                 <h2>This is Mekhi</h2>
