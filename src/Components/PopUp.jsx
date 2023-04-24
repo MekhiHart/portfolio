40217e6282ed8a950062ea113bf017e8e7129d19
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-24 09:43:48 -0700

    added seperator between tabs

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -21,16 +21,23 @@ export default function PopUp(props){
 
         if (isClicked){
             console.log("here")
-            buttonStyle = {backgroundColor:"white" }
+            buttonStyle = {backgroundColor:"white", borderTopRightRadius:"10px", borderTopLeftRadius:"10px" }
         }
         else{
             // buttonStyle = {backgroundColor:"green"}
         }
-
-        return <button className="popUp--tab" style={buttonStyle} onClick={() => handleButtonClick(name)}>
+        return  <button className="popUp--tab" style={buttonStyle} onClick={() => handleButtonClick(name)}>
             {<FontAwesomeIcon icon={buttonIcons[index]} size='1x'/>} {name}
             </button>
     })
+
+    // * seperators
+    const seperator = <div id="tab--seperator"></div>
+    buttons.splice(1,0, seperator)
+    buttons.splice(3,0, seperator)
+    buttons.splice(5,0, seperator)
+    buttons.splice(7,0, seperator)
+    
 
     return(
         <div className="popup--overlay">
