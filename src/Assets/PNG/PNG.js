e82188990938ac90c8208ec9837488535f824bf1
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-27 11:41:53 -0700

    added some png to test

---
+++ src/Assets/PNG/PNG.js
@@ -0,0 +1,15 @@
+import Python from "./Python.png"
+import JavaScript from "./JavaScript.png"
+import Java from "./Java.png"
+import React from "./React.png"
+import GitHub from "./GitHub.png"
+import MongoDB from "./GitHub.png"
+
+export default {
+    Python,
+    JavaScript,
+    Java,
+    React,
+    GitHub,
+    MongoDB
+}
