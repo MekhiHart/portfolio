b29ad2818fd863cd7778eb42a067e99cf533fa5a
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-27 16:48:02 -0700

    attempt to make nav buttons be responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,10 +4,10 @@ export default function NavBar(props){
     const [heroDiv,skillsDiv] = scrollDivs // * destructuring from an array
     return(
         <nav id="nav">
-            <a  className="button-30"onClick={() => scrollToDiv(skillsDiv)}>Skills</a>
-            <a className="button-30">Experience</a>
-            <a className="button-30">Projects</a>
-            <a className="button-30">Contact</a>
+            <a  className="button-30"onClick={() => scrollToDiv(skillsDiv)}><span>Skills</span></a>
+            <a className="button-30"><span>Projects</span></a>
+            <a className="button-30"><span>Experience</span></a>
+            <a className="button-30"><span>Contact</span></a>
         </nav>
     )
 }
