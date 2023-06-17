60260f4423e8271f49a31c285f89ea5c5dfe7ee6
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-17 13:32:43 -0700

    fixed responsivess of iframe

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -19,7 +19,7 @@ export default function Projects(){
                 <div className="project--information">
                     <u><h3 style={{paddingLeft:"10px", fontSize:"24px"}}>{project.projectName}</h3></u>
 
-                    <div>
+                    <div >
                         {techStack}
                     </div>
 
