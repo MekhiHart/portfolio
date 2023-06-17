9f8a1edb500ed117069bfc2dd02e1fae307bac6f
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-17 16:25:45 -0700

    changed about me to about

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar(props){
     const [heroDiv, aboutDiv, skillsDiv,projectsDiv] = scrollDivs
     return(
         <nav id="nav">
-            <a  className="button-30"onClick={() => scrollToDiv(aboutDiv)}><span>About Me</span></a>
+            <a  className="button-30"onClick={() => scrollToDiv(aboutDiv)}><span>About</span></a>
             <a  className="button-30"onClick={() => scrollToDiv(skillsDiv)}><span>Skills</span></a>
             <a className="button-30"onClick={() => scrollToDiv(projectsDiv)}><span>Projects</span></a>
             <a className="button-30"><span>Contact</span></a>
