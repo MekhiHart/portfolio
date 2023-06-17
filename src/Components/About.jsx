44ce023312cd25bc6c0c37457be53fd27ed174d0
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-16 19:57:52 -0700

    changed some words in the about me section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,7 +11,7 @@ export default function About(){
                 <img id="about--container--profile" src={Profile} />
                 <span>I am a first-generation student that immigrated in the United States from the Philippines.<br/><br/>
                  Going through the struggle of adapting into a new country developed my tenacity to 
-                 solve challenging problems that compliments my skills in Software Engineering
+                 solve challenging problems that compliments my skills as a Software Engineering
                  <br/><br/> However when I'm not working on a project, I love cooking food that I find in Instagram 
                  with my partner
                   </span>
