c1bd625bc81055268744cc8c21f521af4d70bc80
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-19 23:57:06 -0700

    changed up nav bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,10 +4,10 @@ export default function NavBar(props){
     const [heroDiv, aboutDiv, skillsDiv,projectsDiv,contactDiv] = scrollDivs
     return(
         <nav id="nav">
-            <a  className="button-30"onClick={() => scrollToDiv(aboutDiv)}><span>About</span></a>
-            <a  className="button-30"onClick={() => scrollToDiv(skillsDiv)}><span>Skills</span></a>
-            <a className="button-30"onClick={() => scrollToDiv(projectsDiv)}><span>Projects</span></a>
-            <a className="button-30"onClick={() => scrollToDiv(contactDiv)}><span>Contact</span></a>
+            <a  className=""onClick={() => scrollToDiv(aboutDiv)}><span>About</span></a>
+            <a  className=""onClick={() => scrollToDiv(skillsDiv)}><span>Skills</span></a>
+            <a className=""onClick={() => scrollToDiv(projectsDiv)}><span>Projects</span></a>
+            <a className=""onClick={() => scrollToDiv(contactDiv)}><span>Contact</span></a>
         </nav>
     )
 }
