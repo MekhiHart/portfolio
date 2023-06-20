ef3c782df3d11bcc0b19dcbb9c21eba79b45e996
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-19 19:57:39 -0700

    added resume component

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom"
+import pdfFile from "../Assets/Software Engineer Resume 2023.pdf"
 export default function Resume(){
     return(
-        <div>
-            <Link to="/">Home</Link>
+        <div className="resume--wrapper">
+            <h1>Resume</h1>
+            <iframe id="resume--pdf" src={pdfFile}/>
+            <div>
+                <Link className="button-30 project--source" to="/">Back</Link>
+                <a className="button-30 project--source" target="_blank" href="https://drive.google.com/file/d/12Kqr3mP8Z75CRPphWRdP-PGQisb6zUsJ/view">Download Resume</a>
+            </div>
         </div>
     )
 
