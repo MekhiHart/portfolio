eb7af6104bae8e7f475c8f9dd1bf957f48c76546
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-23 08:59:59 -0700

    changes applied to dark and light mode, need to find color to hover that suits both colors

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -25,7 +25,13 @@ export default function PopUp(props){
     const {setPopUp, popUpData, handlePopUpData, handleTabButtons, tabButtons} = props
     const buttonIcons = [faLaptopCode, faReact, faDatabase, faScrewdriverWrench]
 
-    const textColor = isLightMode ? "white" : "black"
+    const tabSelectedColor = isLightMode ? "rgb(238, 237, 231)" : "#545c6d"
+    const tabColor = isLightMode ? "#DCD2CC" : "#333842"
+    const textColor = isLightMode ? "black" : "white"
+
+    const searchHeaderColor = isLightMode ? " #F9F9FB" : "#6a768a"
+    const tabHeader = searchHeaderColor
+    const mainColor = isLightMode ? "rgb(238, 237, 231)" : "#21252b"
     const icon = isLightMode ? faSun : faMoon
 
     const buttons = tabButtons.map((buttonObj, index) =>{
@@ -33,10 +39,10 @@ export default function PopUp(props){
         let buttonStyle = {}
 
         if (isClicked){
-            buttonStyle = {backgroundColor:"#EEEDE7", borderTopRightRadius:"10px", borderTopLeftRadius:"10px", color:"black"}
+            buttonStyle = {backgroundColor:tabSelectedColor, borderTopRightRadius:"10px", borderTopLeftRadius:"10px", color:textColor}
         }
         else{
-            // buttonStyle = {backgroundColor:"green"}
+            buttonStyle = {backgroundColor:tabColor, color:textColor}
         }
         return  <button className="popUp--tab" style={buttonStyle} onClick={() => handleButtonClick(name)}>
             {<FontAwesomeIcon icon={buttonIcons[index]} size='1x'/>} <span className="popUp--tab--name">{name}</span>
@@ -71,22 +77,22 @@ export default function PopUp(props){
     return(
         <div className="popUp--overlay" id="modal--overlay">
             <div className='modal' id='modal'>
-                <div className="popUp--tabs">
+                <div className="popUp--tabs" style={{backgroundColor:tabHeader}} >
                     {buttons}
-                    <button onClick={closeModal} id="closePopUp"><FontAwesomeIcon icon={faX} size='1x'/></button>
+                    <button style={{backgroundColor:tabHeader}} onClick={closeModal} id="closePopUp"><FontAwesomeIcon icon={faX} size='1x'/></button>
                 </div>
 
-                <div  className="popUp--container"> 
-                    <div id="popUp--header">
+                <div style={{backgroundColor:mainColor, color:textColor}}  className="popUp--container"> 
+                    <div  id="popUp--header">
                         <div className='popUp--header--left'>
                             <FontAwesomeIcon className="popUp--header--icon" style={{marginLeft:"10px", marginRight:"10px"}}  icon={faArrowLeft} size='1x'/>
                             <FontAwesomeIcon className="popUp--header--icon" style={{marginRight:"20px"}}  icon={faArrowRight} size='1x'/>
                             <FontAwesomeIcon className="popUp--header--icon" style={{marginRight:"15px"}} icon={faRotateRight} size='1x'/>
-                            <FontAwesomeIcon className="popUp--header--icon" icon={faHouse} size='1x'/>
+                            <FontAwesomeIcon onClick={() => setIsLightMode(prevState => !prevState)} id="switch--mode" className="popUp--header--icon" icon={icon} size='1x'/>
                         </div>
 
                         <div className='popUp--header--right'>
-                            <div id="popUp--header--search">
+                            <div style={{backgroundColor:searchHeaderColor}} id="popUp--header--search">
                                 <FontAwesomeIcon id="popUp--header--search--icon" className="popUp--header--icon" icon={faCircleInfo} size='1x'/>
                                 <span id="popUp--header--search--url">www.MekhiHart_isCool.dev</span>
                             </div>
