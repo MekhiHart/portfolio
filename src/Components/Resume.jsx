c00b219c75e4f80b8715e6cf5af22f3190a8f08f
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-25 17:44:18 -0700

    updated resume links

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
 import pdfFile from "../Assets/Software Engineer Resume.pdf"
 export default function Resume(){
-    const pdfLink = "https://drive.google.com/file/d/1Ry7Bcak_bsac80Vk7SwJqWfngCdGdp3R/view"
+    const pdfLink = "https://drive.google.com/file/d/1YwB6NeqqyBizR8VyxQnYp7giN8ufp2LK/view"
+    const embedLink = "https://drive.google.com/file/d/1YwB6NeqqyBizR8VyxQnYp7giN8ufp2LK/preview"
     return(
         <div className="resume--wrapper">
             <h1>Resume</h1>
-            <iframe id="resume--pdf" src={pdfFile}/>
+            <iframe id="resume--pdf" src={embedLink}/>
             <div>
                 <Link className="button-30 project--source" to="/">Back</Link>
                 <a className="button-30 project--source" target="_blank" href={pdfLink}>Download Resume</a>
