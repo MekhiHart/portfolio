ea93ae4f49140e8e5623c86ec0f0cb00e6f9deef
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-25 17:21:13 -0700

    updated href link for pdf download

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
 import pdfFile from "../Assets/Software Engineer Resume.pdf"
 export default function Resume(){
+    const pdfLink = "https://drive.google.com/file/d/1Ry7Bcak_bsac80Vk7SwJqWfngCdGdp3R/view"
     return(
         <div className="resume--wrapper">
             <h1>Resume</h1>
             <iframe id="resume--pdf" src={pdfFile}/>
             <div>
                 <Link className="button-30 project--source" to="/">Back</Link>
-                <a className="button-30 project--source" target="_blank" href="https://drive.google.com/file/d/12Kqr3mP8Z75CRPphWRdP-PGQisb6zUsJ/view">Download Resume</a>
+                <a className="button-30 project--source" target="_blank" href={pdfLink}>Download Resume</a>
             </div>
         </div>
     )
