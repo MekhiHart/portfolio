f0167ff6b3ac3c522dcef23f25fb4c540db9ed80
MekhiHart <mekhihartdelacruz@gmail.com>
2023-06-25 18:36:24 -0700

    added another new resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 import pdfFile from "../Assets/Software Engineer Resume.pdf"
 export default function Resume(){
-    const pdfLink = "https://drive.google.com/file/d/1YwB6NeqqyBizR8VyxQnYp7giN8ufp2LK/view"
-    const embedLink = "https://drive.google.com/file/d/1YwB6NeqqyBizR8VyxQnYp7giN8ufp2LK/preview"
+    const pdfLink = "https://drive.google.com/file/d/1tLauRmPK_irDtssJW_YZlZZTWdVfz1hX/view"
+    const embedLink = "https://drive.google.com/file/d/1tLauRmPK_irDtssJW_YZlZZTWdVfz1hX/preview"
     return(
         <div className="resume--wrapper">
             <h1>Resume</h1>
