834e1307a75699327abbc5a27f423bf7f1344bc1
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-03-23 16:39:01 -0700

    removed fun fact

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,9 +12,6 @@ export default function About(){
 
                     <span id="about--container--description">
                         <b style={{fontSize:"24px", fontWeight:"600"}}>About Me:</b> <br></br> Hello! I'm Mekhi Hart. I'm a first-generation immigrant and student. I immigrated from the Philippines when I was 13 for better opportunities. Bringing my ideas to life is what inspired me to pursue Computer Science.
-                        <br/> <br/>
-                        <b>Fun fact</b>: In the history of CSULB, I am the <u>first person</u> to become a Software Engineer for <span  style={{color:"#e50a14", fontFamily: "Bebas Neue", fontWeight:"bold", letterSpacing:"2px", fontSize:"20px"}}>Netflix</span>.
-
                     </span>
     
                 </div>
